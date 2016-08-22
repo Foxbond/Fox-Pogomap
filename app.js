21aecb6bf4e5e811f817d5db4ddc920fc6532899
Foxbondpl <foxbondpl@gmail.com>
2016-08-22 15:26:11 +0200

    Begin

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
 /**
  * http://usejsdoc.org/
  */
+
+var winston = require("winston");
+var log = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({'timestamp':function (){
+    	  var d = new Date();
+    	  return '['+('0'+d.getHours()).slice(-2)+':'+('0'+d.getMinutes()).slice(-2)+':'+('0'+d.getSeconds()).slice(-2)+']';
+      },'colorize':true})
+    ]
+});
+
+var cfg = require('config.json');
+
+if (!cfg.logConsole) {
+	log.info("Removed logging to console!");
+	log.remove(log.transports.Console);
+}
+
+if (cfg.logFile) {
+	log.info("Added logging to file ("+cfg.logFilename+")");
+	log.add(log.transports.File, { filename: cfg.logFilename, timestamp:true });
+}
+log.level = cfg.logLevel;
+
+
+log.info('Loading libraries');
+
+//here
+
+
+/** App **/
+
+//here
